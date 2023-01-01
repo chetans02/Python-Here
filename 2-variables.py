483efacbbfb9639f3f77b473ab5e19569dd949b2
Chetan Sapkal <99116561+chetans02@users.noreply.github.com>
2023-01-01 21:05:32 +0530

    Add files via upload

---
+++ 2-variables.py
@@ -0,0 +1,86 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2+3
+5
+ a  = 3
+ 
+SyntaxError: unexpected indent
+4*6
+24
+4?2
+SyntaxError: invalid syntax
+4/2
+2.0
+5//2
+2
+
+2 **3
+8
+2**4
+16
+prin('Chetan\'s "Laptop"')
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    prin('Chetan\'s "Laptop"')
+NameError: name 'prin' is not defined. Did you mean: 'print'?
+print('Chetan\'s "Laptop"')
+Chetan's "Laptop"
+print('c:\docs\chetan')
+c:\docs\chetan
+print('c:\docs\navin')
+
+c:\docs
+avin
+x = 2
+y = 3
+print(x+y)
+5
+x+y
+5
+x = 9
+x+Y
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    x+Y
+NameError: name 'Y' is not defined. Did you mean: 'y'?
+x+y
+12
+>>>   _+y
+...   
+SyntaxError: unexpected indent
+>>> _+y
+15
+>>> name = 'Chetan'
+>>> name
+'Chetan'
+>>> name + 'rocks'
+'Chetanrocks'
+>>> 'Chetanrocks'name + 'rocks'
+... 
+SyntaxError: invalid syntax
+>>> name + ' rocks'
+'Chetan rocks'
+>>> name[0]
+'C'
+>>> name[4]
+'a'
+>>> name[7]
+Traceback (most recent call last):
+  File "<pyshell#29>", line 1, in <module>
+    name[7]
+IndexError: string index out of range
+>>> name[-1]
+'n'
+>>> name[0:2]
+'Ch'
+>>> name[1:4]
+'het'
+>>> name[1:]
+'hetan'
+>>> name[:5]
+'Cheta'
+>>> myname= 'Chetan Sapkal'
+>>> len(my name)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> len(myname)
+13
