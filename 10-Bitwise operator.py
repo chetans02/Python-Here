1ac5608aeb8bc2a8173b6caf534ccfd5f78d9dfe
Chetan Sapkal <99116561+chetans02@users.noreply.github.com>
2023-01-02 21:51:28 +0530

    Add files via upload

---
+++ 10-Bitwise operator.py
@@ -0,0 +1,25 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ~12
+-13
+>>> ~45
+-46
+>>> 12 & 13
+12
+>>> 12 | 13
+13
+>>> 25&30
+24
+>>> 
+>>> 12^1
+13
+>>> 12^13
+1
+>>> 25^30
+7
+>>> 
+>>> #Left shift
+>>> 10<<2
+40
+>>> 10>>2
+2
