ed0056804f9339173f0fd0a6a801b9f9ca3c9917
Chetan Sapkal <99116561+chetans02@users.noreply.github.com>
2023-01-03 19:04:26 +0530

    Add files via upload

---
+++ 11-math function.py
@@ -0,0 +1,27 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> x = math.sqrt(25)
+>>> x
+5.0
+>>> x = math.sqrt(15)
+... 
+>>> x
+3.872983346207417
+>>> print(math.floor(2.9))
+2
+>>> print(math.ceil(2.2))
+3
+>>> 3**2
+9
+>>> print(math.pow(3,2))
+9.0
+>>> prnt(math.pi)
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    prnt(math.pi)
+NameError: name 'prnt' is not defined. Did you mean: 'print'?
+>>> print(math.pi)
+3.141592653589793
+>>> print(math.e)
+2.718281828459045
---
+++ 12-User input.py
@@ -0,0 +1,10 @@
+#x = int(input("Enter 1st no"))
+#y = int(input("Enter 2nd no"))
+#z = x + y
+#print(z)
+
+#ch = input("enter a char")
+#print (ch[0])''
+
+result = eval(input("enter an expr"))
+print(result)
