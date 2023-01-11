63bea8f4476cdc8ffb55029a068976343aecda17
Chetan Sapkal <99116561+chetans02@users.noreply.github.com>
2023-01-11 22:54:30 +0530

    Add files via upload

---
+++ 34-Module- Acessing Calc model.py
@@ -0,0 +1,5 @@
+from Calcmodel import *
+a = int(input("Enter a no"))
+b = int(input("Enter a no"))
+c = add(a,b)
+print(c)
---
+++ 35-Class.py
@@ -0,0 +1,16 @@
+
+#1
+'''
+class Computers:
+    def config(self):
+        print("i5,16gb,1TB")
+com1 = Computers()
+com2 = Computers()
+
+Computers.config(com1)
+Computers.config(com2)
+
+com1.config()
+com2.config() '''
+
+
